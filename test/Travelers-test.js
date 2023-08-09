d663487e0e0b817f8d69745fe50ff0dba9806cf7
Prissilla Escobar <prissilla988@gmail.com>
2023-08-09 10:05:45 -0600

    Feat: create tests for travelers tests

--- test/Travelers-test.js
+++ test/Travelers-test.js
@@ -1,3 +1,33 @@
-import { expect } from 'chai';
+import { expect } from 'chai'
 
-import {sampleTravelersData} from './src/sample-data/sampleTravelersData.js'
+import {
+    getUserData,
+} from '../src/data-model'
+
+import {sampleTravelersData} from '../src/sample-data/sampleTravelersData'
+
+describe('User Information', () => {
+    let currentUser, invalidDataList, invalidDataListTest
+
+    beforeEach(() => {
+        invalidDataList = {users:[{}]}
+    })
+
+    it('Should return user info by ID', function() {
+        let userOne = getUserData(1, sampleTravelersData)
+        let userTwo = getUserData(3, sampleTravelersData)
+
+        expect(userOne).to.be.an('object')
+        expect(userOne.name).to.equal("Ham Leadbeater")
+        expect(userOne.travelerType).to.equal("relaxer")
+        expect(userTwo).to.be.an('object')
+        expect(userTwo.name).to.equal("Sibby Dawidowitsch")
+        expect(userTwo.travelerType).to.equal("shopper")
+    })
+
+    it('Should return Invalid User if user does not exist', () => {
+        let invalidUser = getUserData(62, sampleTravelersData)
+    
+        expect(invalidUser).to.equal('Invalid User')
+      })
+})
