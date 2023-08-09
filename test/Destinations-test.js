472a6a00a5fc289989d00c7ef64d6dcff21db4ba
Prissilla Escobar <prissilla988@gmail.com>
2023-08-09 10:05:23 -0600

    Feat: create test functions

--- test/Destinations-test.js
+++ test/Destinations-test.js
@@ -1,3 +1,3 @@
 import { expect } from 'chai';
 
-import {sampleDestinationsData} from './src/sample-data/sampleDestinationsData.js';
+import {sampleDestinationsData} from '../src/sample-data/sampleDestinationsData';
