6b8415d78e5d6a3d6095ff82c32c2becc5d1b943
Prissilla Escobar <prissilla988@gmail.com>
2023-08-08 19:43:54 -0600

    Refactor: took out the fetch call for traveler by ID since I am not working on the login page

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,14 +3,13 @@ export const fetchUserData = (dataType) => {
     .then(res => res.json())
 }
 
-export const fetchTravelerById = (userId) => {
-    return fetch(`http://localhost:3001/api/v1/travelers/${userId}`)
-}
+// export const fetchTravelerById = (userId) => {
+//     return fetch(`http://localhost:3001/api/v1/travelers/${userId}`)
+// }
 
 export const promises = [
     fetchUserData('travelers'),
     fetchUserData('trips'),
     fetchUserData('destinations'),
-    fetchUserData('travelers'),
-    fetchTravelerById(userId)
+    // fetchTravelerById(userId)
 ]
